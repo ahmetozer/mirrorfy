3a68aefc3491e70eecafabd2337e9539b10ad617
Ahmet OZER <git@ahmetozer.org>
2021-02-11 22:40:59 +0300

    adduser each container creation

--- docker_cmd.sh
+++ docker_cmd.sh
@@ -3,16 +3,18 @@
 port=${port-22}
 user=${user-mirrorfy}
 
-if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
-    if [ ! -z "$port" ]; then
-        port_regex="^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([1-9][0-9]{3})|([1-9][0-9]{2})|([1-9][0-9])|([1-9]))$"
+if [ ! -z "$port" ]; then
+    port_regex="^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([1-9][0-9]{3})|([1-9][0-9]{2})|([1-9][0-9])|([1-9]))$"
 
-        if [[ ! $port =~ $port_regex ]]; then
-            echo "ERR : Please write valid port number != $remote_port"
-            exit 1
-        fi
-        #echo "Port $port" >>/etc/ssh/sshd_config
+    if [[ ! $port =~ $port_regex ]]; then
+        echo "ERR : Please write valid port number != $remote_port"
+        exit 1
     fi
+    #echo "Port $port" >>/etc/ssh/sshd_config
+fi
+
+if [ -f "first_run" ]; then
+    set -e
     if [ ! -z "$user" ]; then
         if [[ ! $user =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9]+$|^[a-zA-Z0-9_.+-]+$ ]]; then
             echo 'Your user is has a unexpected char. You can write a email adress or user'
@@ -26,9 +28,12 @@ if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
         echo "$password
 $password" | passwd $user
     fi
+    set +e
+    rm first_run
+fi
 
+if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
     ssh-keygen -A
-
 fi
 
 if [ -f "/authorized_keys" ]; then
