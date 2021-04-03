febc3360a2996c021a47a061b434e3f339f4c62d
Ahmet OZER <git@ahmetozer.org>
2021-04-04 02:07:45 +0300

    unlock account and no motd

--- docker_cmd.sh
+++ docker_cmd.sh
@@ -20,8 +20,9 @@ if [ ! -z "$port" ]; then
 fi
 
 if [ -f "first_run" ]; then
-    set -e
+    echo "first run"
     if [ ! -z "$user" ]; then
+        echo "user $user"
         if [[ ! $user =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9]+$|^[a-zA-Z0-9_.+-]+$ ]]; then
             echo 'Your user is has a unexpected char. You can write a email adress or user'
             exit 1
@@ -30,11 +31,31 @@ if [ -f "first_run" ]; then
                 echo "user '$user' is found"
             else
                 adduser --disabled-password $user
+                if [ "$?" != "0" ]; then
+                    echo "error while creating user '$user'"
+                    exit 1
+                fi
             fi
+        fi
+    fi
+    echo "unlocking user '$user'"
+    passwd -u $user > /dev/null 2>&1
+
+    if [ -f "/authorized_keys" ]; then
+        echo "file /authorized_keys found"
+        ssh_dir=$(eval echo ~$user/.ssh)
+        mkdir -p $ssh_dir
+        cat /authorized_keys >$ssh_dir/authorized_keys
+        chown -R $user $ssh_dir
+        chmod 644 $ssh_dir/authorized_keys
+    else
+        if [ ! -z "$password" ]; then
+            echo "Error: '/authorized_keys' were not found and the password was not defined. No one can be login to the system."
         fi
     fi
 
     if [ ! -z "$password" ]; then
+        echo "Changing password for '$user'"
         echo "$password
 $password" | passwd $user
     fi
@@ -51,15 +72,8 @@ if [ "$auto" == "yes" ]; then
     echo "Username $user password $password"
 fi
 
-if [ -f "/authorized_keys" ]; then
-    ssh_dir=$(eval echo ~$user/.ssh)
-    mkdir -p $ssh_dir
-    cat /authorized_keys >$ssh_dir/authorized_keys
-    chown -R $user $ssh_dir
-    chmod 644 $ssh_dir/authorized_keys
-fi
 cp /etc/sshd/* /etc/ssh/
 
 ssh_path=$(command -v sshd)
 echo "Starting sshd"
-$ssh_path -D -p $port
+$ssh_path -D -p $port -o PrintMotd=no
